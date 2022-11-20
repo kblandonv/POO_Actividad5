2ed84df579b938d38993c5a9f41bd9165e29f4af
asmejiah <111844634+asmejiah@users.noreply.github.com>
2022-11-19 19:50:00 -0500

    Add files via upload

---
+++ EjercicioMenús/Corriente.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package appmenus;
+
+public class Corriente {
+    public static double calcular_corriente(double voltaje,double resistencia){
+     double corriente;
+     corriente = voltaje / resistencia;
+     return corriente;
+ }  
+}
---
+++ EjercicioMenús/Pitagoras.java
@@ -0,0 +1,18 @@
+package appmenus;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Asdruval Mejia
+ */
+public class Pitagoras {
+public static double calcular_hipotenusa(double catetoa, double catetob){
+ double hipotenusa;
+ hipotenusa = Math.sqrt(Math.pow(catetoa,2) + Math.pow(catetob,2));
+ return hipotenusa;
+}
+}
