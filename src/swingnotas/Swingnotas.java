c5b773c958e0074e3590e720b1dcc22f839f9d01
user <user@DESKTOP-P4CUQ3D>
2022-11-19 23:23:04 -0500

    ejercicio 8.2

---
+++ src/swingnotas/Swingnotas.java
@@ -0,0 +1,13 @@
+
+package swingnotas;
+
+
+public class Swingnotas {
+
+
+    public static void main(String[] args) {
+        Ventana window=new Ventana();
+        window.setVisible(true);
+    }
+    
+}
