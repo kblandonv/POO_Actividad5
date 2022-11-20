6551ff64222f737e29e7481564b366a1d09aab81
asmejiah <111844634+asmejiah@users.noreply.github.com>
2022-11-19 19:48:49 -0500

    Add files via upload

---
+++ PuntoMenús/Voltaje.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package appmenus;
+
+public class Voltaje {
+ public static double calcular_voltaje(double corriente,double resistencia){
+     double voltaje;
+     voltaje = corriente * resistencia;
+     return voltaje;
+ }
+}
